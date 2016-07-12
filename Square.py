4200bd5e1d5ccc6ca4c14cb377c4cb480bc3c4f3
WCartagenaASC <wesleycartagena23@gmail.com>
2016-07-12 11:43:16 -0400

    Changeable square
    A script in which when you change the squaresize number the size of the
    square will change

---
+++ Square.py
@@ -0,0 +1,10 @@
+from Myro import *
+init ("sim")
+squaresize = 5
+forward (1, squaresize)
+turnBy (90)
+forward (1, squaresize)
+turnBy (90)
+forward (1, squaresize)
+turnBy (90)
+forward (1, squaresize)
