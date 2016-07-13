bbb44b3e41bed93c5fc241649ec7174c2d7bd608
WCartagenaASC <wesleycartagena23@gmail.com>
2016-07-13 13:44:01 -0400

    Me Initials
    7/13/16
    Defining variable and parameters

---
+++ Letter C.py
@@ -0,0 +1,9 @@
+from Myro import * 
+init("sim")
+penDown()
+
+def c():
+    turnBy(180)
+    motors (1, 0, 10)
+    
+c()
---
+++ Letter W.py
@@ -0,0 +1,16 @@
+from Myro import * 
+init("sim")
+penDown()
+def w():
+    turnBy(120)
+    forward(1, 3)
+    backward(1, 3)
+    turnBy(300)
+    forward (1,1.5)
+    turnBy (250)
+    forward (1,1.5)
+    turnBy(120)
+    forward(1,3)
+    
+    
+w()
