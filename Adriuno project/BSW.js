804f6afd2abc89d5e1687bcdb04f561fbec93b6f
WCartagenaASC <wesleycartagena23@gmail.com>
2016-08-08 22:11:44 -0400

    for steven
    dont do anything stupid

---
+++ Adriuno project/BSW.js
@@ -0,0 +1,7 @@
+function changeImage() {
+	if (document.getElementById("lock").src == "img/portfolio/lock.png") {
+		document.getElementById("lock").src == "img/portfolio/unlock.png";
+	} else {
+		document.getElementById("lock").src == "img/portfolio/lock.png";
+	}
+}
